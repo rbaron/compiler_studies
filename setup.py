e3229c77e1c33cf0018d226f1cc08d349abf3d26
rbaron <rbaron@rbaron.net>
2018-09-08 12:41:06 +0200

    Proper packaging so imports are easier and cleaner

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="compiler_studies",
+    version="0.0.1",
+    author="rbaron",
+    author_email="rbaron@rbaron.net",
+    description="Collection studies on compilers",
+    packages=setuptools.find_packages(),
+)
